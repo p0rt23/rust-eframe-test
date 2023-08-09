df2fd3df7f2a4c71767ddced68385887352a7be2
Mark Smith <msmith4@gmail.com>
2023-08-09 08:09:30 -0500

    no-prompt on package install

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev libxkbcommon-dev libssl-dev
+sudo apt-get install -y libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev libxkbcommon-dev libssl-dev
 rustup update
 rustup target add wasm32-unknown-unknown
 cargo install --locked trunk
